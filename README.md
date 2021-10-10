d8c568125e0aa3d9d17e41b42f3cc82d392ccd38
NickSpirakis <nicholasespirakis@lewisu.edu>
2021-10-10 13:39:14 -0500

    added bounds changed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # google-maps-hello-world-ns
+# this is where I found the syntax for adding listeners notifications to the screen: 
+# https://www.techstrikers.com/GoogleMap/Code/how-to-add-bounds-changed-event-listener-to-google-map.php
--- index.js
+++ index.js
@@ -5,6 +5,14 @@ function initMap() {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
+
+  gMap.addListener('bounds_changed', () => {
+    document.getElementById('panel').innerHTML = 'Bounds Changed';
+    window.setTimeout(() => {
+      document.getElementById('panel').innerHTML = ' ';
+    }, 2000);
+    console.log('bounds changed');
+  });
 }
 
 function initApplication(){
